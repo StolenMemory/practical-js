22a0b4ebd03f2bf477c21c7f727af8018f38d56f
StolenMemory <stolenmemory@protonmail.com>
2018-09-11 11:31:55 +0200

    version 0.5

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
 let todoList = {
     todos: [],
     displayTodos: function () {
-        console.log ("My todos: ", this.todos);
-    }, // this is a method (a function which is a property of an object) that displays todos.. since we don't have variables set up in an object, we use this keyword to access properties in current object
-    addTodo: function (newTodo) {
+        if (this.todos.length === 0) {
+            console.log ("The list is empty")
+        } else {
+            console.log ("My todos: ");
+            for (let i = 0; i < this.todos.length; i++) {
+                if (this.todos.completed === true) {
+                    console.log ("(X)", this.todos[i].todoText);
+                } else {
+                    console.log ("( )", this.todos[i].todoText);
+                }
+            }
+        }
+    }, // now the method only display todo text instead of whole object and it also shows its completition status (as text), and also tells you if you don't have any objects in your list
+    addTodo: function (todoText) {
         this.todos.push ({
-            newTodo: newTodo,
+            todoText: todoText,
             completed: false
         });
         this.displayTodos()
     }, // this method now adds objects which can be used to store various datas and not just plain text, it also stores info whether the todo is completed or not
-    editTodo: function (position, newTodo) {
-        this.todos[position].newTodo = newTodo;
+    editTodo: function (position, todoText) {
+        this.todos[position].todoText = todoText;
         this.displayTodos();
-    }, // method edits a function at given position with the newTodo value (just for the name/text, it won't edit whole object, just the name of it) and then displays todo (to be practical and see changes immediately)
+    }, // method edits a function at given position with the todoText value (just for the name/text, it won't edit whole object, just the name of it) and then displays todo (to be practical and see changes immediately)
     deleteTodo: function (position) {
         this.todos.splice (position, 1);
         this.displayTodos();
