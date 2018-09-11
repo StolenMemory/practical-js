08babfd487e23d718e16892935ecb9a2e026c4fa
StolenMemory <stolenmemory@protonmail.com>
2018-09-11 22:19:08 +0200

    version 0.10

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 let todoList = {
     todos: [],
-    addTodo: function(todoText) {
-        this.todos.push ({
+    addTodo: function (todoText) {
+        this.todos.push({
             todoText: todoText,
             completed: false
         });
     }, // this method now adds objects which can be used to store various datas and not just plain text, it also stores info whether the todo is completed or not
-    editTodo: function(position, todoText) {
+    editTodo: function (position, todoText) {
         this.todos[position].todoText = todoText;
     }, // method edits a function at given position with the todoText value (just for the name/text, it won't edit whole object, just the name of it) and then displays todo (to be practical and see changes immediately)
-    deleteTodo: function(position) {
+    deleteTodo: function (position) {
         this.todos.splice(position, 1);
     }, // this method deletes a todo at given position
-    toggleCompleted: function(position) {
+    toggleCompleted: function (position) {
         let todo = this.todos[position];
         todo.completed != todo.completed;
     }, // this method takes a position of todo we want, and then flips the property of it (completed) to opposite of what it currently is
-    toggleAll: function() {
+    toggleAll: function () {
         let totalTodos = this.todos.length;
         let completedTodos = 0;
         for (let i = 0; i < totalTodos; i++) {
@@ -35,16 +35,16 @@ let todoList = {
         } // if at least one item is not completed, it marks all as true
     } // this method toggles all objects completed or vice versa
 }; // this is an object which has property that is set to an array of items, it's a good thing to be programming with OOP in mind (object oriented programming) = cleaner code
-   // it also has a few methods which execute things and then always display the list for clarity and practicality
+// it also has a few methods which execute things and then always display the list for clarity and practicality
 
-let inputHandlers =  {
-    addTodo: function() {
+let inputHandlers = {
+    addTodo: function () {
         let addTodoTextInput = document.getElementById("addTodoTextInput");
         todoList.addTodo(addTodoTextInput.value);
         addTodoTextInput.value = "";
         userView.displayTodos();
     },
-    editTodo: function() {
+    editTodo: function () {
         let editTodoPositionInput = document.getElementById("editTodoPositionInput");
         let editTodoTextInput = document.getElementById("editTodoTextInput");
         todoList.editTodo(editTodoPositionInput.valueAsNumber, editTodoTextInput.value);
@@ -52,26 +52,24 @@ let inputHandlers =  {
         editTodoTextInput.value = "";
         userView.displayTodos();
     },
-    deleteTodo: function() {
-        let deleteTodoPositionInput = document.getElementById("deleteTodoPositionInput");
-        todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
-        deleteTodoPositionInput.value = "";
+    deleteTodo: function (position) {
+        todoList.deleteTodo(position);
         userView.displayTodos();
     },
-    toggleCompleted: function() {
+    toggleCompleted: function () {
         let toggleCompletedPositionInput = document.getElementById("toggleCompletedPositionInput");
         todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
         deleteTodoPositionInput.value = "";
         userView.displayTodos();
     },
-    toggleAll: function() {
+    toggleAll: function () {
         todoList.toggleAll();
         userView.displayTodos();
     }
 }; // created a new object that will handle input from user and run methods accordingly
 
 let userView = {
-    displayTodos: function() {
+    displayTodos: function () {
         let todosUl = document.querySelector("ul");
         todosUl.innerHTML = "";
         for (let i = 0; i < todoList.todos.length; i++) {
@@ -83,8 +81,28 @@ let userView = {
             } else {
                 todoTextWithStatus = "( ) " + todo.todoText
             }
+            todoLi.id = i;
             todoLi.textContent = todoTextWithStatus;
+            todoLi.appendChild(this.createDeleteButton());
             todosUl.appendChild(todoLi);
         }
     }, // this method looks for unordered list in .html and then creates a list item in it for each item in our todo list and shows its name and status
+    createDeleteButton: function () {
+        let deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        deleteButton.className = "deleteButton";
+        return deleteButton;
+    },
+    setUpEventListeners: function () {
+        let todosUl = document.querySelector("ul");
+        todosUl.addEventListener("click", function (event) {
+            let elementClicked = event.target;
+            if (elementClicked.className === "deleteButton") {
+                inputHandlers.deleteTodo(parseInt(elementClicked.parentNode.id));
+            }
+        });
+    } // we have set up method for event listeners in general.. it listens to clicks on whole ul element and then decides based on the event it recorded
+      // (when clicked on element with class delete - it will run the delete method)
 }; // this object handles output for user to view, we no longer need to have any looks into console, so we deleted our previous viewing functionalities that outputed to console
+
+userView.setUpEventListeners(); // this just runs the function we need
