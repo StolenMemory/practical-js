4615e4f55ea91adb2c96c0baa44a9311734e6111
StolenMemory <stolenmemory@protonmail.com>
2018-09-11 14:43:53 +0200

    version 0.8

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
 let todoList = {
     todos: [],
-    displayTodos: function () {
+    displayTodos: function() {
         if (this.todos.length === 0) {
-            console.log ("The list is empty")
+            console.log("The list is empty")
         } else {
-            console.log ("My todos: ");
+            console.log("My todos: ");
             for (let i = 0; i < this.todos.length; i++) {
                 if (this.todos.completed === true) {
-                    console.log ("(X)", this.todos[i].todoText);
+                    console.log("(X)", this.todos[i].todoText);
                 } else {
-                    console.log ("( )", this.todos[i].todoText);
+                    console.log("( )", this.todos[i].todoText);
                 }
             }
         }
     }, // now the method only display todo text instead of whole object and it also shows its completition status (as text), and also tells you if you don't have any objects in your list
-    addTodo: function (todoText) {
+    addTodo: function(todoText) {
         this.todos.push ({
             todoText: todoText,
             completed: false
         });
         this.displayTodos()
     }, // this method now adds objects which can be used to store various datas and not just plain text, it also stores info whether the todo is completed or not
-    editTodo: function (position, todoText) {
+    editTodo: function(position, todoText) {
         this.todos[position].todoText = todoText;
         this.displayTodos();
     }, // method edits a function at given position with the todoText value (just for the name/text, it won't edit whole object, just the name of it) and then displays todo (to be practical and see changes immediately)
-    deleteTodo: function (position) {
-        this.todos.splice (position, 1);
+    deleteTodo: function(position) {
+        this.todos.splice(position, 1);
         this.displayTodos();
     }, // this method deletes a todo at given position
-    toggleCompleted: function (position) {
+    toggleCompleted: function(position) {
         let todo = this.todos[position];
         todo.completed != todo.completed;
         this.displayTodos();
     }, // this method takes a position of todo we want, and then flips the property of it (completed) to opposite of what it currently is
-    toggleAll: function () {
+    toggleAll: function() {
         let totalTodos = this.todos.length;
         let completedTodos = 0;
         for (let i = 0; i < totalTodos; i++) {
@@ -56,13 +56,33 @@ let todoList = {
 }; // this is an object which has property that is set to an array of items, it's a good thing to be programming with OOP in mind (object oriented programming) = cleaner code
    // it also has a few methods which execute things and then always display the list for clarity and practicality
 
-   let displayTodosButton = document.getElementById ("displayTodosButton") //these adds variables for buttons (by button ID declared in .html) so we can work with them
-   let toggleAllButton = document.getElementById ("toggleAllButton")
-
-   displayTodosButton.addEventListener ("click", function() { // these will listen for a click on a button and then run the appropriate function
-    todoList.displayTodos();
-   });
-
-   toggleAllButton.addEventListener ("click", function() {
-    todoList.toggleAll();
-   });
+let inputHandlers =  {
+    displayTodos: function() {
+        todoList.displayTodos();
+    },
+    addTodo: function() {
+        let addTodoTextInput = document.getElementById("addTodoTextInput");
+        todoList.addTodo(addTodoTextInput.value);
+        addTodoTextInput.value = "";
+    },
+    editTodo: function() {
+        let editTodoPositionInput = document.getElementById("editTodoPositionInput");
+        let editTodoTextInput = document.getElementById("editTodoTextInput");
+        todoList.editTodo(editTodoPositionInput.valueAsNumber, editTodoTextInput.value);
+        editTodoPositionInput.value = "";
+        editTodoTextInput.value = "";
+    },
+    deleteTodo: function() {
+        let deleteTodoPositionInput = document.getElementById("deleteTodoPositionInput");
+        todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
+        deleteTodoPositionInput.value = "";
+    },
+    toggleCompleted: function() {
+        let toggleCompletedPositionInput = document.getElementById("toggleCompletedPositionInput");
+        todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
+        deleteTodoPositionInput.value = "";
+    },
+    toggleAll: function() {
+        todoList.toggleAll();
+    }
+}; // created a new object that will handle input from user and run methods accordingly
